e796251ab117d297d70433722efb619d57445f5a
michael <michael85731@gmail.com>
2016-12-29 03:42:43 +0800

    add scale picture function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from subprocess import call
 from tkinter import *
+from PIL import Image, ImageTk
 
 # 設定window
 window = Tk()
@@ -9,34 +10,38 @@ window.attributes("-fullscreen", True)
 window.attributes("-topmost", True)
 window.focus_force()
 
-# 顯示起始圖片(第一張圖片)
-photo = PhotoImage(file="balloon(1).gif")
-label = Label(window, image=photo)
-label.config(width=500)
-label.config(height=500)
-label.pack()
+# 設定起始變數
+raw_image = Image.open("balloon(1).gif")
+target_image = ImageTk.PhotoImage(raw_image)
+label = Label(window, image=target_image)
+ratio = min( window.winfo_width() / raw_image.size[0], window.winfo_height() / raw_image.size[1])
+index = 1
+
+# 開啟起始圖片
+def switch_image(e):
+  global index
+  global ratio
+
+  if index < 16:
+    fileName = "balloon(" + str(index) + ").gif"
+    raw_image = Image.open(fileName)
+    raw_image = raw_image.resize( (int(raw_image.size[0] * ratio), int(raw_image.size[0] * ratio)), Image.BILINEAR )
+    target_image = ImageTk.PhotoImage(raw_image)
+    label.image = target_image
+    label.config(image = target_image)
+    label.pack()
+
+    index += 1
+  #else:
+    #play_video()
+    #index = 2
 
 # 播放影片
 def play_video():
   call(["omxplayer", "-o", "local", "qqbz.mp4"])
 
-# 設定接到訊號時的方法
-index = 2
-def switch_image(e):
-  global index
+window.bind("<KeyPress>", switch_image) # 設定接到訊號時的方法
 
-  if index < 16:
-    fileName = "balloon(" + str(index) + ").gif"
-    newPhoto = PhotoImage(file=fileName)
-    label.config(image = newPhoto)
-    label.image = newPhoto
-
-    index += 1
-  else:
-    play_video()
-    index = 2
-
-# 設定接到訊號時的方法
-window.bind("<KeyPress>", switch_image)
+switch_image(0) # 執行第一次
 
 window.mainloop()
